94f3f1353d9a08b7170e368563e4a349234ea9cf
Kathleen Pehl <kathleen.pehl@gmail.com>
2020-10-28 09:50:53 -0400

    changed connection.js back to starter code configuration; initialized database in mysql and seeded with provided seed file; wrote GET all categories route and tested in Insomnia Core

--- config/connection.js
+++ config/connection.js
@@ -4,19 +4,14 @@ require('dotenv').config();
 // Sequelize constructor
 const Sequelize = require('sequelize');
 
-let sequelize;
-
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-    host: 'localhost',
-    dialect: 'mysql',
-    port: 3306,
-    dialectOptions: {
-      decimalNumbers: true,
-    }
-  })
-}
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+      host: 'localhost',
+      dialect: 'mysql',
+      dialectOptions: {
+        decimalNumbers: true,
+      },
+    });
 
 module.exports = sequelize;
--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -6,6 +6,28 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  Category.findAll({
+    attributes: [
+      'id',
+      'category_name'
+    ],
+    include: [
+      {
+        model: Product,
+        attributes: [
+          'id',
+          'product_name',
+          'price',
+          'stock',
+          'category_id'
+        ]
+      }
+    ]
+  })
+  .then(dbProductData => res.json(dbProductData))
+  .catch(err => {
+    res.status(500).json(err)
+  })
 });
 
 router.get('/:id', (req, res) => {
