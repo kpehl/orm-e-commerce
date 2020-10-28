565328480810d6924eb6e105b8d9ba177f2cd14d
Kathleen Pehl <kathleen.pehl@gmail.com>
2020-10-28 09:07:11 -0400

    modified connection.js for server and JAWSDB connection; created sequelize models for Category, Product, Product Tag, and Tag per the challenge instructions

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,22 @@
+// Dependencies
+// dotenv for environmental variable configuration
 require('dotenv').config();
-
+// Sequelize constructor
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+let sequelize;
+
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+    dialectOptions: {
+      decimalNumbers: true,
+    }
+  })
+}
 
 module.exports = sequelize;
